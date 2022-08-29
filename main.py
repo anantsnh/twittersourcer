73c410cdb0919d03558277e9a1f9a1e579d62b9f
Anant <anantsnh@umich.edu>
2022-08-28 21:36:05 -0400

    Add flask capability

--- main.py
+++ main.py
@@ -1,46 +1,63 @@
 import tweepy
 import gspread
 import time
+from flask import Flask
+from flask import jsonify
 
-# Sheet being used as db: https://docs.google.com/spreadsheets/d/1gf6DMcryo7ldYNgRPvT6XFQkIBEtcPZwXyWTU1Z4Yyc/edit?usp=sharing
-sa = gspread.service_account(filename = "service_account.json")
-sh = sa.open("twitterSourcer")
-wks = sh.worksheet("Data")
-allInvestors = sh.worksheet("Investors")
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    # Sheet being used as db: https://docs.google.com/spreadsheets/d/1gf6DMcryo7ldYNgRPvT6XFQkIBEtcPZwXyWTU1Z4Yyc/edit?usp=sharing
+    sa = gspread.service_account(filename = "service_account.json")
+    sh = sa.open("twitterSourcer")
+    wks = sh.worksheet("Data")
+    allInvestors = sh.worksheet("Investors")
 
 
-# set the ID for this week
-lastWeeksID = wks.acell('A' + str(wks.row_count)).value
-thisWeeksID = int(lastWeeksID) + 1
+    # set the ID for this week
+    lastWeeksID = wks.acell('A' + str(wks.row_count)).value
+    thisWeeksID = int(lastWeeksID) + 1
 
-# API keys for Twitter
-api_key = "oFtEvDTIr5RNXApaB16Ilkve5"
-api_secrets = "b2XBs8YZZfxW71Wa51hxi04wUdd3yFDIzQMsIbwcTVe884R9Fc"
-access_token = "769270874721624064-gH3qwZKSNuZC2M3uz3XtjJBafQVKu0r"
-access_secret = "QrCfFYjqkjLrIDKzJKSJqsX57uxhHMmG1SKz4EvuMjSeJ"
+    # API keys for Twitter
+    api_key = "oFtEvDTIr5RNXApaB16Ilkve5"
+    api_secrets = "b2XBs8YZZfxW71Wa51hxi04wUdd3yFDIzQMsIbwcTVe884R9Fc"
+    access_token = "769270874721624064-gH3qwZKSNuZC2M3uz3XtjJBafQVKu0r"
+    access_secret = "QrCfFYjqkjLrIDKzJKSJqsX57uxhHMmG1SKz4EvuMjSeJ"
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(api_key,api_secrets)
-auth.set_access_token(access_token,access_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(api_key,api_secrets)
+    auth.set_access_token(access_token,access_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
-# Making sure that this shit works
-try:
-    api.verify_credentials()
-    print('Successful Authentication')
-except:
-    print('Failed authentication')
+    # Making sure that this shit works
+    try:
+        api.verify_credentials()
+        print('Successful Authentication')
+    except:
+        print('Failed authentication')
 
-numRows = allInvestors.row_count
-for i in range (0, numRows):
-    # get investor info from sheets
-    twitterHandle = allInvestors.acell('A' + str((i + 1))).value
-    vcName = allInvestors.acell('B' + str((i + 1))).value
-    
-    # get latest 5 each investor twitter account follows
-    twitterUser = api.get_user(screen_name = twitterHandle)
-    twitterFollows = twitterUser.friends()
-    
-    for i in range (0, 5):
-        wks.append_row([thisWeeksID, (str(twitterFollows[i].screen_name) + '_' + vcName), twitterFollows[i].screen_name, vcName, twitterFollows[i].description, str(twitterUser.screen_name), twitterUser.description])
-    time.sleep(5)
+    numRows = allInvestors.row_count
+    for i in range (0, numRows):
+        # get investor info from sheets
+        twitterHandle = allInvestors.acell('A' + str((i + 1))).value
+        vcName = allInvestors.acell('B' + str((i + 1))).value
+        
+        # get latest 5 each investor twitter account follows
+        twitterUser = api.get_user(screen_name = twitterHandle)
+        twitterFollows = twitterUser.friends()
+        
+        for i in range (0, 5):
+            followedUser = twitterFollows[i].screen_name
+            followedTwitterLink = str('https://twitter.com/' + followedUser)
+            investorName = twitterUser.screen_name
+            investorTwitterLink = str('https://twitter.com/' + investorName)
+
+            investorHyperLink = f'=HYPERLINK(\"{investorTwitterLink}\",\"{investorName}\")'
+            followedUserHyperlink = f'=HYPERLINK(\"{followedTwitterLink}\",\"{followedUser}\")'
+
+            wks.append_row([thisWeeksID, (str(twitterFollows[i].screen_name) + '_' + vcName), followedUserHyperlink, vcName, twitterFollows[i].description, investorHyperLink, twitterUser.description], value_input_option='USER_ENTERED')
+        time.sleep(5)
+
+    resp = jsonify(success=True)
+    return resp
